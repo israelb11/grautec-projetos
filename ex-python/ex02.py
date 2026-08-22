12d5f8246c7a1649f1c9dca111055c9946f0e7eb
Your Name <you@example.com>
2026-08-22 15:59:08 -0300

    Ex02 / Ex01

---
+++ ex-python/ex02.py
@@ -0,0 +1,8 @@
+def calcNum(num1, num2):
+    total = num1 + num2
+    return total
+
+num1 = int(input("digite o primeiro numero: "))
+num2 = int(input("digite o segundo numero: "))
+retorno = calcNum(num1, num2)
+print(f"O resultado é: {retorno}")
