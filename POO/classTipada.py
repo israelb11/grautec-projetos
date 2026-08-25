5e52f74dbd2a2d28ddebd0e75815f203818658ad
Your Name <you@example.com>
2026-08-25 20:09:33 -0300

    refatoração de codigo

--- POO/classTipada.py
+++ POO/classTipada.py
@@ -6,10 +6,15 @@ class UsuarioMutavel:
     nome: str
     idade: int
 
+print(f"""------------------------------\n
+USUARIO MUTAVEL-1\n""")
 p1 = UsuarioMutavel("Ana", 25)
-print(p1)
-p1.idade = 26  # Permitido!
-print(p1)
+print(f"O nome do user é: {p1.nome} e a sua idade é {p1.idade} \n")
+
+print(f"""------------------------------\n
+USUARIO MUTAVEL-2\n""")
+p1 = UsuarioMutavel("Carlos", 32)
+print(f"O nome do user é: {p1.nome} e a sua idade é {p1.idade} \n")
 
 # Classe tipada imutável (frozen=True)
 @dataclass(frozen=True)
@@ -18,5 +23,10 @@ class UsuarioImutavel:
     idade: int
 
 p2 = UsuarioImutavel("Bruno", 30)
-print(p2)
-# p2.idade = 31  # Lança FrozenInstanceError!
+print(f"""------------------------------\n
+USUARIO IMUTAVEL-1\n""")
+print(f"""O nome do user é: {p2.nome} e a sua idade é {p2.idade} \n
+------------------------------ \n""")
+
+p2.nome = "Alberto"
+p2.idade = 31  # Lança FrozenInstanceError!
--- ex-python/ex01.py
+++ ex-python/ex01.py
@@ -1,3 +1,3 @@
-nome1 = input("Digite o primeiro nome")
-nome2 = input("Digite o segundo nome")
-print(f"primeiro nome {nome01}, {nome02}")
+nome1 = input("Digite o primeiro nome: ")
+nome2 = input("Digite o segundo nome: ")
+print(f"primeiro nome: {nome1}, segundo nome: {nome2}")
