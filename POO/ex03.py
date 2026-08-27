1970227f96dd595b9f2be3c12c974ff5ba46d863
Your Name <you@example.com>
2026-08-27 19:03:09 -0300

    Refinamento atividades

---
+++ POO/ex03.py
@@ -0,0 +1,12 @@
+class ex03:
+    nome = "Hudson"
+    turma = "INF13-N"
+
+    def DigitaNome(self):
+        nome = input("digite seu nome: ")  
+        print(f" Seu nome é: {nome}")      
+
+p1 = ex03()
+p1.DigitaNome()
+
+    
