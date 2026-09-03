e219d385caf7c7cd67380744f3e4c27bad33ba0a
Your Name <you@example.com>
2026-09-03 19:32:19 -0300

    “att”

--- POO/CalcularData.py
+++ POO/CalcularData.py
@@ -8,6 +8,7 @@ class DataValidator:
             self.leapYear = 0
 
     def receiveData(self):
+        print("----------------------------")
         self.day = int(input("Digite o dia: "))
         self.mouth = int(input("Digite o mês: "))
         self.year = int(input("Digite o ano: "))
@@ -17,34 +18,40 @@ class DataValidator:
         # Validação ano bisexto
         if (self.mouth == 2):
             if (self.leapYear != 0) and (self.day > 28):
-                print("ERROR! DATA INVALIDA!")
+                print("----------------------------")
+                print("ERROR! DATA INVALIDA!\n")
                 print("Este ano não é bisexto, os dias de fevereiro vão somente até 28.")
                 print("Insira uma data valida.")
                 return
             if (self.leapYear == 0) and (self.day > 29):
-                print("ERROR! DATA INVALIDA!") 
+                print("----------------------------")
+                print("ERROR! DATA INVALIDA!\n") 
                 print("Este ano é bisexto, os dias de fevereiro vão somente até 29.")
                 print("Insira uma data valida.")
 
         if (self.day > 31) or (self.day < 1):
-            print("ERROR! DATA INVALIDA!")
-            print("Não existe nenhum mês que possua esse dia preenchido.")
-            print("Insira uma data valida.")
+            print("----------------------------")
+            print("ERROR! DATA INVALIDA!\n")
+            print("Não existe nenhum mês que possua esse dia.")
+            print("Insira um dia valida.")
             return
         
-        if (self.mouth > 12):
-            print("ERROR! DATA INVALIDA!")
+        if (self.mouth < 1) or (self.mouth > 12):
+            print("----------------------------")
+            print("ERROR! DATA INVALIDA!\n")
             print("Não existe esse mês!")
-            print("Insira uma data valida.")
+            print("Insira uma mẽs valido.")
             return
         
         if (self.year < 1):
-            print("ERROR! DATA INVALIDA!")
+            print("----------------------------")
+            print("ERROR! DATA INVALIDA!\n")
             print("Não existe ano negativo.")
             print("Insira uma data valida.")
             return
 
     def showResult(self):
+        print("----------------------------")
         print(f"""{self.day}/{self.mouth}/{self.year}""")
         print("Ano bisexo" if (self.leapYear == 0) else "Ano não bisexto")
 
